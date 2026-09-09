61600475bb6e5f83c25ca74f14a9915134d4a8aa
vamshiadla1910 <vamshiadla1910@gmail.com>
2026-09-09 17:44:52 +0530

    Initial

---
+++ src/components/pages/Home/Learner_Journey/Learner_Journey.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserManagementTable.css';
+
+const UserManagementTable = () => {
+  return (
+    <div className="usermanagementtable">
+      {/* TODO: implement UserManagementTable */}
+    </div>
+  );
+};
+
+export default UserManagementTable;
