93dbdc112cbe1636bc5f2b61dca6276f94975cf3
vamshiadla1910 <vamshiadla1910@gmail.com>
2026-09-11 14:07:14 +0530

    1st merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import Home from "./modules/marketing/presentation/pages/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./modules/marketing/presentation/components/Header/Header";
+import Footer from "./modules/marketing/presentation/components/Footer/Footer";
+
+import Home from "./modules/marketing/presentation/pages/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+
       <Routes>
-  <Home/>
+        <Route path="/" element={<Home />} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
 }
