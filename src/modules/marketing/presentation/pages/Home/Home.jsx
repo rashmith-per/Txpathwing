99d22db8af3fb2f3a7da8c1584d6fdb83af1dfcb
vamshiadla1910 <vamshiadla1910@gmail.com>
2026-09-11 10:26:23 +0530

    Update One Platform section UI

---
+++ src/modules/marketing/presentation/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import OnePlatform from '../../components/OnePlatform/OnePlatform'
+
+function Home() {
+
+  return (
+    <>
+    <OnePlatform/>
+     </>
+  )
+}
+
+export default Home
