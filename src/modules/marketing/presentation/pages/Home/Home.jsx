35ef70e3728baa40eeb7b5059eced9257d7fea39
vadlaharinath <vadlaharinath22@gmail.com>
2026-09-11 10:32:02 +0530

    courses

---
+++ src/modules/marketing/presentation/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Courses from "../Courses/Courses";
+
+function Home() {
+  return (
+    <div >
+        <Courses/>
+
+    </div>
+  );
+}
+
+export default Home;
