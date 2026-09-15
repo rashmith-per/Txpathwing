3a78315f1d3327024f0f0e04ce88261772eae791
bommalavishnu321 <vishnubommala2003@gmail.com>
2026-09-15 17:39:10 +0530

    NEW UPDATES

--- src/modules/marketing/presentation/pages/Home/Home.jsx
+++ src/modules/marketing/presentation/pages/Home/Home.jsx
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 import OnePlatform from "../../components/OnePlatform/OnePlatform";
-import ElevenStages from "../Learner_journey/Learner_journey";
+import LearnerJourney from "../Learner_journey/Learner_journey";
 import Courses from "../Courses/Courses";
 import CTA from "../../components/CTA/CTA";
-=======
-
-import LearnerJourney from "../Learner_journey/Learner_journey";
-
-
-
-
-import CTA from "../../components/CTA/CTA";
-import OnePlatfrom from "../../components/OnePlatform/OnePlatform";
-import "./Home.css";
->>>>>>> 030362553fbc18fa5b02349719c22c8ece528015
 import Mode from "../../components/Modes/Modes";
 import FAQ from "../../components/FAQ/FAQ";
 import "./Home.css";
@@ -22,12 +10,11 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-<<<<<<< HEAD
       {/* One Platform Section */}
       <OnePlatform />
 
       {/* Learner Journey Section */}
-      <ElevenStages />
+      <LearnerJourney />
 
       {/* Courses Section */}
       <Courses />
@@ -39,24 +26,6 @@ const Home = () => {
       <Mode />
 
       {/* FAQ Section */}
-=======
-      {/* One Platform */}
-      <OnePlatfrom />
-
-      {/* Learner Journey */}
-    <LearnerJourney />
-
-      {/* Courses */}
-      <Cources />
-
-      {/* CTA */}
-      <CTA />
-
-      {/* Modes */}
-      <Mode />
-
-      {/* FAQ */}
->>>>>>> 030362553fbc18fa5b02349719c22c8ece528015
       <FAQ />
     </>
   );
